463c62fb85cce2e6144a38c615ff2b76ca7738cd
avenal <avenalszarosiwy@gmail.com>
2018-09-03 17:14:53 +0200

    added basic form

--- itSystem/main/views.py
+++ itSystem/main/views.py
@@ -4,4 +4,11 @@ from . import forms
 
 def index(request):
     form = forms.ContactForm()
+
+    if request.method == 'POST':
+        form = forms.FormName(request.POST)
+
+        if form.is_valid():
+            print("Validation Success!")
+            print(form.cleaned_data['name'])
     return render(request,'main/index.html', {'form': form})
