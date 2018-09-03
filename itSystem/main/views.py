dbc880e768316798851678b69956847b43f5fc3f
avenal <avenalszarosiwy@gmail.com>
2018-09-03 16:34:29 +0200

    added urls.py for main app

--- itSystem/main/views.py
+++ itSystem/main/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from . import forms
 # Create your views here.
+
+def index(request):
+    form = forms.ContactForm()
+    return render(request,'main/index.html', {'form': form})
